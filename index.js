e639ad6f65423a5ad626eb4816649f7abed75b8c
jotacollantes <juan@collantes.ec>
2022-09-10 00:49:34 -0500

    index corregido

--- index.js
+++ index.js
@@ -31,9 +31,9 @@ app.use('/api/auth',require('./routes/auth'))
 //*Rutas CRUD
 app.use('/api/events',require('./routes/events'))
 
-app.get('*',(req,res))=>{
+app.get('*',(req,res)=>{
     res.sendFile(__dirname+'/public/index.html');
-}
+})
 
 
 
